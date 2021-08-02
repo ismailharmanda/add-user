2a42b5aeffc7cfd3b9cb4858b0f60cf114a69fca
ismail harmanda <iharmanda@hotmail.com>
2021-08-02 19:51:09 +0300

    ErrorModal done

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,8 +1,21 @@
 import React from "react";
 import Card from "./Card";
+import Button from "./Button";
 
 const ErrorModal = (props) => {
-  return <Card></Card>;
+  return (
+    <Card>
+      <header>
+        <h2>{props.title}</h2>
+      </header>
+      <div>
+        <p>{props.message}</p>
+      </div>
+      <footer>
+        <Button>Okay</Button>
+      </footer>
+    </Card>
+  );
 };
 
 export default ErrorModal;
