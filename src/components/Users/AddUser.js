82b7adb1e684ddb337ed3ee3e9e03a37cc53b44b
ismail harmanda <iharmanda@hotmail.com>
2021-08-02 18:06:48 +0300

    create input state and collect all data

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
+  const [user, setUser] = useState({ username: "", age: "" });
   const onSubmitForm = (e) => {
     e.preventDefault();
+  };
+  const onInputChange = (e) => {
+    console.log(user);
+    e.target.id === "username" &&
+      setUser((prev) => {
+        return { ...prev, username: e.target.value };
+      });
+    e.target.id === "age" &&
+      setUser((prev) => {
+        return { ...prev, age: e.target.value };
+      });
   };
   return (
     <Card className={classes.input}>
       <form onSubmit={onSubmitForm}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={onInputChange} />
         <label htmlFor="age">Age</label>
-        <input id="age" type="text" />
+        <input id="age" type="text" onChange={onInputChange} />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
