d0e0de7458c694dc6af9058bfbf0f4222a707b5c
ismail harmanda <iharmanda@hotmail.com>
2021-08-02 20:02:13 +0300

    background css implemented

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -5,17 +5,20 @@ import Button from "./Button";
 import classes from "./ErrorModal.module.css";
 const ErrorModal = (props) => {
   return (
-    <Card className={classes.modal}>
-      <header className={classes.header}>
-        <h2>{props.title}</h2>
-      </header>
-      <div className={classes.content}>
-        <p>{props.message}</p>
-      </div>
-      <footer className={classes.actions}>
-        <Button>Okay</Button>
-      </footer>
-    </Card>
+    <div>
+      <div className={classes.backdrop} />
+      <Card className={classes.modal}>
+        <header className={classes.header}>
+          <h2>{props.title}</h2>
+        </header>
+        <div className={classes.content}>
+          <p>{props.message}</p>
+        </div>
+        <footer className={classes.actions}>
+          <Button>Okay</Button>
+        </footer>
+      </Card>
+    </div>
   );
 };
 
