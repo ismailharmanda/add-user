6d91ced63c73dc9898efd7be6c5a4f3e02b11927
ismail harmanda <iharmanda@hotmail.com>
2021-08-02 17:19:49 +0300

    create Card.js, implement css to card and use a shell for AddUser.js

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,17 +1,20 @@
 import React from "react";
+import Card from "../UI/Card";
 
 const AddUser = (props) => {
   const onSubmitForm = (e) => {
     e.preventDefault();
   };
   return (
-    <form onSubmit={onSubmitForm}>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text" />
-      <label htmlFor="age">Age</label>
-      <input id="age" type="text" />
-      <button type="submit">Add User</button>
-    </form>
+    <Card>
+      <form onSubmit={onSubmitForm}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age</label>
+        <input id="age" type="text" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 };
 
