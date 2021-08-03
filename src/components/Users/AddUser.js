736d588fdf56ab10f53fe52f0e5bbc175e585c12
ismail harmanda <iharmanda@hotmail.com>
2021-08-03 19:33:28 +0300

    catch the values via useRef

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -7,47 +7,39 @@ import classes from "./AddUser.module.css";
 const AddUser = (props) => {
   const usernameInputRef = useRef();
   const ageInputRef = useRef();
-  const [user, setUser] = useState({
-    username: "",
-    age: "",
-    id: Math.random().toString(),
-  });
   const [error, setError] = useState();
   const onSubmitForm = (e) => {
     e.preventDefault();
-    if (user.username.trim().length === 0 || user.age.trim().length === 0) {
+    const enteredUsername = usernameInputRef.current.value;
+    const enteredAge = ageInputRef.current.value;
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
       setError({
         title: "Invalid Input",
         message: "Please enter a valid name and age(non-empty values).",
       });
       return;
     }
-    if (+user.age < 1) {
+    if (+enteredAge < 1) {
       setError({
         title: "Invalid Input",
         message: "Please enter a valid age(bigger than 0).",
       });
       return;
     }
-    if (isNaN(+user.age) || !Number.isInteger(+user.age)) {
+    if (isNaN(+enteredAge) || !Number.isInteger(+enteredAge)) {
       setError({
         title: "Invalid Input",
         message: "Please enter a valid age(only integers).",
       });
       return;
     }
-    props.onAddUser(user);
-    setUser({ username: "", age: "", id: Math.random().toString() });
-  };
-  const onInputChange = (e) => {
-    e.target.id === "username" &&
-      setUser((prev) => {
-        return { ...prev, username: e.target.value };
-      });
-    e.target.id === "age" &&
-      setUser((prev) => {
-        return { ...prev, age: e.target.value };
-      });
+    props.onAddUser({
+      username: enteredUsername,
+      age: enteredAge,
+      id: Math.random().toString(),
+    });
+    usernameInputRef.current.value = "";
+    ageInputRef.current.value = "";
   };
   const hideModal = () => {
     setError(null);
@@ -64,21 +56,9 @@ const AddUser = (props) => {
       <Card className={classes.input}>
         <form onSubmit={onSubmitForm}>
           <label htmlFor="username">Username</label>
-          <input
-            id="username"
-            type="text"
-            onChange={onInputChange}
-            value={user.username}
-            ref={usernameInputRef}
-          />
+          <input id="username" type="text" ref={usernameInputRef} />
           <label htmlFor="age">Age</label>
-          <input
-            id="age"
-            type="text"
-            onChange={onInputChange}
-            value={user.age}
-            ref={ageInputRef}
-          />
+          <input id="age" type="text" ref={ageInputRef} />
           <Button type="submit">Add User</Button>
         </form>
       </Card>
