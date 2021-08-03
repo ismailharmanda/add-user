01af6a072d00002350379300693a0c6630b8935b
ismail harmanda <iharmanda@hotmail.com>
2021-08-03 18:54:34 +0300

    backdrop rendered

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,24 +1,37 @@
 import React from "react";
 import Card from "./Card";
 import Button from "./Button";
+import ReactDOM from "react-dom";
 
 import classes from "./ErrorModal.module.css";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.hideModal}></div>;
+};
+const ModalOverlay = (props) => {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.hideModal}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+
 const ErrorModal = (props) => {
   return (
-    <div>
-      <div className={classes.backdrop} onClick={props.hideModal} />
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.hideModal}>Okay</Button>
-        </footer>
-      </Card>
-    </div>
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop hideModal={props.hideModal} />,
+        document.querySelector("#backdrop-root")
+      )}
+    </>
   );
 };
 
